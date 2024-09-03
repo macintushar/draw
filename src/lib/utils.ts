2c57ef92e9bd62a3560f46ae85939cfee98c18b8
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-09-03 16:47:36 +0530

    added date util

---
+++ src/lib/utils.ts
@@ -0,0 +1,32 @@
+import { clsx, type ClassValue } from "clsx";
+import dayjs from "dayjs";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const timeMessages = {
+  default: "👋 Hey",
+  morning: "☀️ Good morning",
+  afternoon: "☀️ Good afternoon",
+  evening: "🌝 Good evening",
+};
+
+export function timeMessage() {
+  let message = timeMessages.default;
+
+  try {
+    const hour = dayjs().get("h");
+
+    if (hour > 4 && hour < 12) message = timeMessages.morning;
+    if (hour >= 12 && hour < 18) message = timeMessages.afternoon;
+    if (hour > 18 && hour < 5) message = timeMessages.evening;
+
+    return message;
+  } catch (error) {
+    console.error(error);
+
+    return message;
+  }
+}
