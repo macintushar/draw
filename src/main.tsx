ba167568d32db25b6c898ef5bc623a12d7cc2b4c
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-09-04 21:09:19 +0530

    added sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
 import "./index.css";
 
@@ -10,6 +11,7 @@ import { routeTree } from "./routeTree.gen";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "./components/theme-provider";
+import NotFound from "./views/NotFound";
 
 const router = createRouter({ routeTree });
 
@@ -19,12 +21,30 @@ declare module "@tanstack/react-router" {
   }
 }
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.tanstackRouterBrowserTracingIntegration(router),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-        <Toaster />
+        <Sentry.ErrorBoundary fallback={<NotFound />}>
+          <RouterProvider router={router} />
+          <Toaster />
+        </Sentry.ErrorBoundary>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
