0e14e08cea6360b81b006c12a5e261b8e28584e1
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-09-04 18:52:07 +0530

    added auto dark or light mode

--- src/views/Page.tsx
+++ src/views/Page.tsx
@@ -1,10 +1,14 @@
 import Loader from "@/components/Loader";
+import { useTheme } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { getDrawData, setDrawData } from "@/db/draw";
 import { Excalidraw } from "@excalidraw/excalidraw";
 import { NonDeletedExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 import { useQuery } from "@tanstack/react-query";
+import { RefreshCcw } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
@@ -16,6 +20,8 @@ export default function Page({ id }: PageProps) {
   const [excalidrawAPI, setExcalidrawAPI] =
     useState<ExcalidrawImperativeAPI | null>(null);
 
+  const { theme } = useTheme();
+
   const { data, isLoading } = useQuery({
     queryKey: ["page", id],
     queryFn: () => getDrawData(id),
@@ -24,13 +30,17 @@ export default function Page({ id }: PageProps) {
   async function updateScene() {
     if (data?.data && excalidrawAPI) {
       const elements = data.data[0].page_elements.elements;
-      excalidrawAPI.updateScene({ elements: elements });
+      excalidrawAPI.updateScene({
+        elements: elements,
+        appState: { viewBackgroundColor: "transparent" },
+      });
       toast("Scene updated");
     }
     if (data?.error) {
       toast("An error occurred", { description: data.error.message });
     }
   }
+  console.log(theme);
 
   async function setSceneData() {
     if (excalidrawAPI) {
@@ -53,16 +63,27 @@ export default function Page({ id }: PageProps) {
 
   return (
     <div className="flex flex-col w-full">
-      <div className="flex gap-2">
-        <Button onClick={setSceneData}>Set Data</Button>
-        <Button onClick={updateScene}>Update Data</Button>
-      </div>
-      <div className="bg-red-900 h-full w-full">
+      <div className="h-full w-full">
         {isLoading ? (
           <Loader />
         ) : (
           <Excalidraw
             excalidrawAPI={(api) => setExcalidrawAPI(api)}
+            renderTopRightUI={() => (
+              <div className="flex gap-2">
+                <div className="flex flex-col space-y-1 w-32">
+                  <Label>Page Name</Label>
+                  <Input></Input>
+                </div>
+                <Button variant="secondary" onClick={setSceneData}>
+                  Save
+                </Button>
+                <Button variant="secondary" size="icon" onClick={updateScene}>
+                  <RefreshCcw className="h-5 w-5" />
+                </Button>
+              </div>
+            )}
+            theme={theme === "dark" ? "dark" : "light"}
             autoFocus
           />
         )}
