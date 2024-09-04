2fa2fef37ae74df655a297371bf93b02717b5248
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-09-04 18:51:39 +0530

    added border

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
     <div className="flex flex-col h-full w-full p-1">
       <Navbar />
       <div className="w-full h-full p-3 pt-1">
-        <div className="flex flex-row gap-8 rounded-xl justify-center h-full bg-gray-200/60 dark:bg-gray-900 py-2 px-3">
+        <div className="flex flex-row gap-8 rounded-xl justify-center h-full bg-gray-200/60 dark:bg-gray-900 py-2 px-3 border-2 dark:border-white border-black">
           <Outlet />
         </div>
       </div>
