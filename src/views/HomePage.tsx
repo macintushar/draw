21b9073546af1aebdd74b8ea9eb9fb9de3549444
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-09-04 20:42:36 +0530

    updated homepage

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -18,15 +18,22 @@ export default function HomePage() {
     }
   }
   return (
-    <main className="h-full w-full">
-      Hello!
-      <Button
-        isLoading={isLoading}
-        loadingText=""
-        onClick={() => action(data ? true : false)}
-      >
-        {data ? "View your pages" : "Sign In"}
-      </Button>
+    <main className="h-full w-full flex justify-center items-center p-2">
+      <div className="sm:w-1/2 w-full flex items-center flex-col gap-y-8 h-fit ">
+        <h1 className="text-6xl font-black">Draw</h1>
+        <h1 className="text-5xl text-center font-medium">
+          The digital drawing tool that enables you to create, edit, and share
+          your drawings across all your devices.
+        </h1>
+        <Button
+          isLoading={isLoading}
+          loadingText=""
+          className="w-2/3 font-semibold text-lg"
+          onClick={() => action(data ? true : false)}
+        >
+          {data ? "View your pages" : "Sign In"}
+        </Button>
+      </div>
     </main>
   );
 }
