5eaa34982db88bc6bbd50b18cd0a347f0923d6d3
smplcod <mplcod2021@gmail.com>
2025-07-08 23:06:22 +0600

    fix: disable TanStackRouterVite in prod to avoid infinite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    ...(process.env.NODE_ENV !== 'production' ? [TanStackRouterVite()] : []),
+  ],
   define: {
     "process.env": {},
   },
